20f84177d288e4bc909a7a79491fad8b43c7ae52
Oleg Frey <olfrey@ya.ru>
2019-02-08 00:31:04 +0300

    70% desktop

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,7 @@ gulp.task('scripts', function() {
 		'src/template/libs/fancybox/dist/jquery.fancybox.js',
 		'src/template/libs/slick-carousel/slick/slick.js',
 		'src/template/libs/jquery.form-styler/dist/jquery.formstyler.js',
+		'src/template/libs/jquery-ui/jquery-ui.js'
 		])
 		.pipe(concat('libs.js')) 
 		.pipe(gulp.dest('src/template/scripts')); 
--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,51 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+	var typesNav = $('.types__nav-list'),
+			typesList = $('.types__list'),
+			typesDots = $('.types__dots'),
+			typesLength = $('.types__item').length,
+			typesCurrent = $('.types__counts .counts__current'),
+			typesTotal = $('.types__counts .counts__total');
+
+	typesTotal.text(typesLength < 10 ? "0" + typesLength : typesLength);
+	typesNav.slick({
+		slidesToShow: 5,
+		arrows: false,
+		asNavFor: typesList,
+		focusOnSelect: true,
+		variableWidth: true
+	});
+
+	typesList.slick({
+		dots: true,
+		appendDots: typesDots,
+		asNavFor: typesNav,
+		fade: true
+	});
+
+	typesList.on('afterChange', function(event, slick, direction){
+		direction++;
+		direction = direction < 10 ? "0" + direction : direction;
+		// direction = typesLength < 10 ? "0" + typesLength : typesLength
+	  typesCurrent.text(direction);
+	});
+
+	function valueElementForm(nameElement) {
+		var newNameElement = '.' + nameElement;
+			element = $(newNameElement);
+		element.each(function(index, el) {
+			var elementInput = $(this).find($('input')),
+				elementLabel = $(this).find($('label')),
+				elementValue = index + 1;
+			elementInput.attr('id', nameElement + '-' + elementValue);
+			elementLabel.attr('for', nameElement + '-' + elementValue);
+		});
+		
+	}
+	valueElementForm('radio');
+	
+	$('select').styler();
+
+	$('.news').tabs();
 });
